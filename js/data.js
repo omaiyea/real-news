a3ad5d28ad81908213c87fa1bc62224e6b1729f7
omaiyea <omaiyea@gmail.com>
2019-08-02 21:37:19 -0700

    return news articles based on search term

--- js/data.js
+++ js/data.js
@@ -1,4 +1,6 @@
 const KEY = '3f6505f5339140c3b9e3721e0cced2ae';
 const ENDPOINT = 'https://newsapi.org/v2/everything?';
+const MAX_RETURN = 15;
+let COUNTER;
 
 const lang = 'language=en';
--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 //get news matching user's input
 function fetchNews(){
-    $('#search-form').submit(function(event){
+    $('#search-form').on('click', 'input[type="submit"]', (function(event){
         event.preventDefault();
+        console.log('clicked');
         let searchTerm = $('input[type="text"]').val();
+        console.log(searchTerm);
         let url = ENDPOINT + `q=` + encodeURIComponent(searchTerm) + `&apiKey=`+ KEY;
         fetch(url)
         .then(response => {
@@ -11,16 +13,31 @@ function fetchNews(){
             }
             throw new Error(response.statusText);
         })
-        .then(responseJson => displayNews(responseJson))
+        .then(responseJson => displayNews(responseJson, searchTerm))
         .catch(err => {
             $('#js-error-message').html(`<p>Something went wrong: ${err.message}</p>`);
         });
-    });
+    }));
 }
 
 //render the articles to the user
-function displayNews(){
-    
+function displayNews(responseJson, term){
+    $('#search-form')[0].reset();
+    $('.results').empty();
+    COUNTER = 1; //start after responseJson index to iterate through max return properly
+    $('.results-description').html(`<h3>Articles about "` + term + `":</h3><br>`);
+    while(!(COUNTER % MAX_RETURN === 0) && COUNTER < responseJson.totalResults){
+         $('.results').append(`<div class="article">
+            <p class="source">` + responseJson.articles[COUNTER-1].source.name + `</p>
+            <h4><a href="` + responseJson.articles[COUNTER-1].url + `" target="_blank">` + responseJson.articles[COUNTER-1].title + `</a></h4>
+            <img src="` + responseJson.articles[COUNTER-1].urlToImage + `" alt="">
+            <p class="description">` + responseJson.articles[COUNTER-1].description + `</p></div>`);
+        COUNTER++;
+    }
+
+  /*  if(COUNTER > responseJson.totalResults){
+        $('.js-error-message').
+    }*/
 }
 
 function handleApp(){
